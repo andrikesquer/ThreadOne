f03f8b195168488203cf241a1ee971e6aa8a20aa
Jorge Chavira <jorgechavira4@gmail.com>
2024-07-09 11:04:00 -0600

    Alineacion de llantas

--- Backend/santiServer.js
+++ Backend/santiServer.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const db = require('mysql2');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const utils = require("./utils");
+// const utils = require("./utils");
 
 const server = express();
 
@@ -11,30 +11,34 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use(cors());
 
-const conn = db.createConnection({
+const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "HCHHPRa4",
     port: 3306,
-    database: "juan"
+    database: "ThreadOne"
 });
+
 
 conn.connect((err) => {
     if (err) {
         console.log("Error connecting to database", err);
-    } else {
-        console.log("Connected to database");
+        throw err;
     }
+    console.log("Connected to database");
 });
 
-server.get("/", (req, res) => {
-    console.log("GET /");
-    res.send("Hola tontito");
+server.get("/", (req, res) => { // Verificacion de conexion con api
+    if (err) {
+        throw err;
+    }
+    console.log("Simple request received");
+    res.send("Simple response delivered");
 });
 
-server.get("/user/:id", (req, res) => {
+server.get("/usuarios/:id", (req, res) => { // Request de usuario especifico
     const id = req.params.id;
-    conn.query("SELECT * FROM users WHERE id_user=?", [id], (error, results) => {
+    conn.query("SELECT * FROM usuarios WHERE id_usuario=?", [id], (error, results) => {
         if (error) {
             console.log("Error fetching data", error);
             res.status(500).send("Error fetching data");
@@ -45,8 +49,8 @@ server.get("/user/:id", (req, res) => {
     });
 });
 
-server.get("/user", (req, res) => {
-    conn.query("SELECT * FROM users", (error, results) => {
+server.get("/usuarios", (req, res) => {     // Request de todos los usuarios
+    conn.query("SELECT * FROM usuarios", (error, results) => {
         if (error) {
             console.log("Error fetching data", error);
             res.status(500).send("Error fetching data");
@@ -57,10 +61,10 @@ server.get("/user", (req, res) => {
     });
 });
 
-server.post("/user", (req, res) => {
-    const { userName, userLastName, email, userPassword } = req.body;
+server.post("/usuarios", (req, res) => { // metodo de insercion de usuario 
+    const { nombre_usuario, apellido_usuario, fecha_nacimiento_usuario, fk_genero, email_usuario, telefono_usuario,contrasena_usuario } = req.body;
     conn.query(
-        "INSERT INTO users (userName, userLastName, email, userPassword ) VALUES (?, ?, ?, ?)",
+        "INSERT INTO users (nombre_usuario, apellido_usuario, fecha_nacimiento_usuario, fk_genero, email_usuario, telefono_usuario,contrasena_usuario) VALUES (?, ?, ?, ?, ?, ?, ?)",
         [userName, userLastName, email, userPassword],
         (error, result) => {
             if (error) {
