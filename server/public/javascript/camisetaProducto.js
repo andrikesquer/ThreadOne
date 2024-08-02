2993f32981a032e4249b3835ce634a0af004c1f9
Jorge Chavira <jorgechavira4@gmail.com>
2024-08-01 20:37:56 -0600

    Uso del javascript para mandar datos a la base de datos de la tabla juan

--- server/public/javascript/camisetaProducto.js
+++ server/public/javascript/camisetaProducto.js
@@ -16,13 +16,21 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".add-to-cart").addEventListener("click", async () => {
         const colorName = document.getElementById("color").value;
         const sizeName = document.getElementById("size").value;
+        const price = parseFloat(document.getElementById("price").textContent.replace('$',''));
+        const name = document.getElementById("nombre").textContent;
         const quantity = 1;
+
+        const imageUrl = document.getElementById("shirt-image").src;
+        const url = new URL(imageUrl);
+        const path = url.pathname;
+
+        // const shirtId = 'shirt_' + Date.now() + '_' + Math.floor(Math.random() * 1000);
 
         const color = colorMap[colorName];
         const size = sizeMap[sizeName];
-
+        
         if (color !== undefined && size !== undefined) {
-            const newItem = { color, size, quantity };
+            const newItem = { name, color, size, quantity, path, price };
 
             try {
                 const response = await fetch('/cart/add', {
@@ -37,11 +45,12 @@ document.addEventListener("DOMContentLoaded", () => {
                     // If the server responded with "Hola mama"
                     const message = await response.text();
                     alert(message);
-
+                    console.log(imageUrl);
                     // Optionally, update the local storage
                     let cart = JSON.parse(localStorage.getItem('cart')) || [];
                     cart.push(newItem);
                     localStorage.setItem('cart', JSON.stringify(cart));
+                    window.location.href="/camisetas"
                 } else {
                     // Handle server errors
                     alert("Failed to add to cart. Server error.");
